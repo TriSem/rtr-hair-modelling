864ccb43e989052df89822c6a00667443d41ce25
TriSem <tristan.semmler@gmx.de>
2020-01-20 13:01:35 +0100

    Added different light types.

--- src/Header Files/Rendering/Light.h
+++ src/Header Files/Rendering/Light.h
@@ -8,11 +8,49 @@ namespace Rendering
 {
 	struct Light
 	{
-		Light();
+		Light() :
+			ambientColor(Vector3::Zero),
+			diffuseColor(Vector3::Zero),
+			specularColor(Vector3::Zero)
+		{}
+
+		~Light() = 0;
+
+		Vector3 ambientColor;
+		Vector3 diffuseColor;
+		Vector3 specularColor;
+	};
+
+	struct DirectionalLight : public Light
+	{
+		DirectionalLight() :
+			direction(Vector3::Zero)
+		{}
+
+		Vector3 direction;
+	};
+
+	struct PointLight : public Light
+	{
+		PointLight() :
+			position(Vector3::Zero),
+			range(1.0f)
+		{}
+
+		Vector3 position;
+		float range;
+	};
+
+	struct SpotLight : public Light
+	{
+		SpotLight()
+		{}
 
 		Vector3 position;
 		Vector3 direction;
-		Color color;
+		Vector3 attenuation;
+		float range;
+		float spotlightExponent;
 	};
 }
 
