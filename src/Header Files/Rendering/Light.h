a08f588d820ee2b21e5c5fda50903a99724d892a
TriSem <tristan.semmler@gmx.de>
2020-02-06 13:34:48 +0100

    Revert "Added different light types."
    This reverts commit 864ccb43e989052df89822c6a00667443d41ce25.

--- src/Header Files/Rendering/Light.h
+++ src/Header Files/Rendering/Light.h
@@ -8,49 +8,11 @@ namespace Rendering
 {
 	struct Light
 	{
-		Light() :
-			ambientColor(Vector3::Zero),
-			diffuseColor(Vector3::Zero),
-			specularColor(Vector3::Zero)
-		{}
-
-		~Light() = 0;
-
-		Vector3 ambientColor;
-		Vector3 diffuseColor;
-		Vector3 specularColor;
-	};
-
-	struct DirectionalLight : public Light
-	{
-		DirectionalLight() :
-			direction(Vector3::Zero)
-		{}
-
-		Vector3 direction;
-	};
-
-	struct PointLight : public Light
-	{
-		PointLight() :
-			position(Vector3::Zero),
-			range(1.0f)
-		{}
-
-		Vector3 position;
-		float range;
-	};
-
-	struct SpotLight : public Light
-	{
-		SpotLight()
-		{}
+		Light();
 
 		Vector3 position;
 		Vector3 direction;
-		Vector3 attenuation;
-		float range;
-		float spotlightExponent;
+		Color color;
 	};
 }
 
