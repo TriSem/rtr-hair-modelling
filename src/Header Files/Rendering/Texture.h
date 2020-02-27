662f8d3897bc38b36b3b48f41cb99d5b44255893
TriSem <tristan.semmler@gmx.de>
2020-02-27 14:15:17 +0100

    Texture class overhaul

--- src/Header Files/Rendering/Texture.h
+++ src/Header Files/Rendering/Texture.h
@@ -8,12 +8,14 @@
 namespace Rendering 
 {
 	struct TextureOptions;
+	enum class TextureType;
 
 	class Texture : public DeviceAccess
 	{
 	public:
 
-		Texture(TextureOptions options);
+		Texture(std::vector<Color> colorData, TextureOptions options);
+		Texture(std::string path, TextureOptions options);
 		ComPtr<ID3D11Texture2D> Get();
 		ComPtr<ID3D11ShaderResourceView> ResourceView();
 		ComPtr<ID3D11SamplerState> SamplerState();
@@ -23,11 +25,27 @@ namespace Rendering
 		ComPtr<ID3D11SamplerState> samplerState = nullptr;
 		ComPtr<ID3D11Texture2D> texture = nullptr;
 		ComPtr<ID3D11ShaderResourceView> resourceView = nullptr;
+		ComPtr<ID3D11RenderTargetView> renderTargetView = nullptr;
+
+		D3D11_TEXTURE2D_DESC CreateTextureDescription(TextureOptions options);
+		void CreateTexture(std::vector<Color> colorData, TextureOptions options);
+		void CreateSamplerState();
+		void CreateViews(TextureType type);
+		void CreateRenderTargetView();
+		void CreateShaderResourceView();
+		virtual void IssueRenderCommands() override;
+	};
+
+	enum class TextureType
+	{
+		RenderTarget,
+		ShaderResource,
+		Mixed
 	};
 
 	struct TextureOptions
 	{
-		std::string path;
+		TextureType type;
 		uint32_t width;
 		uint32_t height;
 	};
--- src/Source Files/Rendering/Texture.cpp
+++ src/Source Files/Rendering/Texture.cpp
@@ -2,11 +2,50 @@
 
 namespace Rendering
 {
-	Texture::Texture(TextureOptions options)
+	Texture::Texture(std::string path, TextureOptions options)
+	{
+		RawFile textureFile(path);
+		std::vector<Color> colorData = textureFile.InterpretAsTexture();
+		CreateTexture(colorData, options);
+		CreateSamplerState();
+		CreateViews(options.type);
+	}
+
+	Texture::Texture(std::vector<Color> colorData, TextureOptions options)
+	{
+		CreateTexture(colorData, options);
+		CreateSamplerState();
+		CreateViews(options.type);
+	}
+
+	ComPtr<ID3D11Texture2D> Texture::Get()
+	{
+		return texture;
+	}
+
+	ComPtr<ID3D11ShaderResourceView> Texture::ResourceView()
+	{
+		return resourceView;
+	}
+
+	ComPtr<ID3D11SamplerState> Texture::SamplerState()
+	{
+		return samplerState;
+	}
+
+	D3D11_TEXTURE2D_DESC Texture::CreateTextureDescription(TextureOptions options)
 	{
 		DXGI_SAMPLE_DESC sampleDescription = {};
 		sampleDescription.Count = 1;
 		sampleDescription.Quality = 0;
+
+		UINT bindFlags = 0;
+		switch (options.type)
+		{
+		case TextureType::RenderTarget: bindFlags |= D3D11_BIND_RENDER_TARGET; break;
+		case TextureType::ShaderResource: bindFlags |= D3D11_BIND_SHADER_RESOURCE; break;
+		case TextureType::Mixed: bindFlags |= D3D11_BIND_SHADER_RESOURCE | D3D11_BIND_RENDER_TARGET; break;
+		}
 
 		D3D11_TEXTURE2D_DESC textureDescription = {};
 		textureDescription.Width = options.width;
@@ -14,23 +53,30 @@ namespace Rendering
 		textureDescription.MipLevels = 1;
 		textureDescription.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
 		textureDescription.SampleDesc = sampleDescription;
-		textureDescription.BindFlags = D3D11_BIND_SHADER_RESOURCE;
+		textureDescription.BindFlags = bindFlags;
 		textureDescription.ArraySize = 1;
 		textureDescription.Usage = D3D11_USAGE_DEFAULT;
-		textureDescription.CPUAccessFlags = D3D11_CPU_ACCESS_READ | D3D11_CPU_ACCESS_WRITE;
+		textureDescription.CPUAccessFlags = 0;
 
-		RawFile textureFile(options.path);
-		std::vector<Color> colorData = textureFile.InterpretAsTexture();
+		return textureDescription;
+	}
 
+	void Texture::CreateTexture(std::vector<Color> colorData, TextureOptions options)
+	{
 		D3D11_SUBRESOURCE_DATA resource = {};
 		resource.pSysMem = colorData.data();
 		resource.SysMemPitch = sizeof(Color) * options.width;
+
+		D3D11_TEXTURE2D_DESC textureDescription = CreateTextureDescription(options);
 
 		MessageAndThrowIfFailed(
 			device->GetDevice()->CreateTexture2D(&textureDescription, &resource, texture.GetAddressOf()),
 			L"Failed to create texture!"
 		);
+	}
 
+	void Texture::CreateSamplerState()
+	{
 		D3D11_SAMPLER_DESC samplerDescription = {};
 		samplerDescription.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
 		samplerDescription.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
@@ -45,7 +91,34 @@ namespace Rendering
 			device->GetDevice()->CreateSamplerState(&samplerDescription, samplerState.GetAddressOf()),
 			L"Failed to create sampler state!"
 		);
+	}
 
+	void Texture::CreateViews(TextureType type)
+	{
+		if (type == TextureType::RenderTarget || type == TextureType::Mixed)
+			CreateRenderTargetView();
+		if (type == TextureType::ShaderResource || type == TextureType::Mixed)
+			CreateShaderResourceView();
+	}
+
+	void Texture::CreateRenderTargetView()
+	{
+		D3D11_TEX2D_RTV subresource;
+		subresource.MipSlice = 0;
+
+		D3D11_RENDER_TARGET_VIEW_DESC viewDescription = {};
+		viewDescription.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+		viewDescription.ViewDimension = D3D11_RTV_DIMENSION_TEXTURE2D;
+		viewDescription.Texture2D = subresource;
+
+		MessageAndThrowIfFailed(
+			device->GetDevice()->CreateRenderTargetView(texture.Get(), &viewDescription, renderTargetView.GetAddressOf()),
+			L"Failed to create shader resource view!"
+		);
+	}
+
+	void Texture::CreateShaderResourceView()
+	{
 		D3D11_TEX2D_SRV srv = {};
 		srv.MipLevels = 1;
 		srv.MostDetailedMip = 0;
@@ -54,23 +127,21 @@ namespace Rendering
 		viewDescription.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
 		viewDescription.ViewDimension = D3D11_SRV_DIMENSION_TEXTURE2D;
 		viewDescription.Texture2D = srv;
-		
+
 		MessageAndThrowIfFailed(
 			device->GetDevice()->CreateShaderResourceView(texture.Get(), &viewDescription, resourceView.GetAddressOf()),
 			L"Failed to create shader resource view!"
 		);
 	}
-	ComPtr<ID3D11Texture2D> Texture::Get()
-	{
-		return texture;
-	}
-	ComPtr<ID3D11ShaderResourceView> Texture::ResourceView()
-	{
-		return resourceView;
-	}
 
-	ComPtr<ID3D11SamplerState> Texture::SamplerState()
+	void Texture::IssueRenderCommands()
 	{
-		return samplerState;
+		if (resourceView != nullptr)
+		{
+			device->GetContext()->PSSetShaderResources(0, 1, resourceView.GetAddressOf());
+		}
+		
+		device->GetContext()->VSSetSamplers(0, 1, samplerState.GetAddressOf());
+		device->GetContext()->PSSetSamplers(0, 1, samplerState.GetAddressOf());
 	}
 }
