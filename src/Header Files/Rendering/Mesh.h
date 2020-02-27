f0ec47683a8aac36f713595dfc1b26d9a0c3879c
TriSem <tristan.semmler@gmx.de>
2020-02-27 13:13:46 +0100

    Moved vertex and index buffers to mesh

--- src/Header Files/Rendering/Mesh.h
+++ src/Header Files/Rendering/Mesh.h
@@ -1,15 +1,41 @@
 #pragma once
 
 #include <vector>
+#include "DeviceAccess.h"
 #include "Vertex.h"
+#include "VertexBuffer.h"
+#include "IndexBuffer.h"
 
 namespace Rendering
 {
 	template<typename VertexType>
-	struct Mesh
+	class Mesh : public DeviceAccess
 	{
 	public:
-		std::vector<VertexType> vertices;
-		std::vector<uint32_t> indices;
+
+		Mesh(const std::vector<VertexType>& vertices, const std::vector<uint32_t>& indices) 
+		{
+			SetTriangles(vertices, indices);
+		}
+
+		void SetTriangles(const std::vector<VertexType>& vertices, const std::vector<uint32_t>& indices)
+		{
+			vertexBuffer = std::make_unique<VertexBuffer<VertexType>>(vertices);
+			indexBuffer = std::make_unique<IndexBuffer>(indices);
+		}
+
+		void BindBuffers()
+		{
+			device->GetContext()->IASetVertexBuffers(0, 1, vertexBuffer->Data().GetAddressOf(), &vertexBuffer->STRIDE, &offset);
+			device->GetContext()->IASetIndexBuffer(indexBuffer->Data().Get(), DXGI_FORMAT_R32_UINT, 0);
+		}
+
+
+	private:
+
+		std::unique_ptr<VertexBuffer<VertexType>> vertexBuffer = nullptr;
+		std::unique_ptr<IndexBuffer> indexBuffer = nullptr;
+
+		static const uint32_t offset = 0;
 	};
 }
